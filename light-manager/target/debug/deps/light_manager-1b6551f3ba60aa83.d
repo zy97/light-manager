3a996f6dd2f02cf81854c3820eccc8d767d6a030
bomky <bom_ky@outlook.com>
2026-07-15 17:34:53 +0800

    first commit

---
+++ light-manager/target/debug/deps/light_manager-1b6551f3ba60aa83.d
@@ -0,0 +1,5 @@
+C:\code-github\light-manager\light-manager\target\debug\deps\light_manager-1b6551f3ba60aa83.d: src\main.rs
+
+C:\code-github\light-manager\light-manager\target\debug\deps\liblight_manager-1b6551f3ba60aa83.rmeta: src\main.rs
+
+src\main.rs:
